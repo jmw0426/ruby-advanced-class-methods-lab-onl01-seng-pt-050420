3e785517766e03ab0fa8d9a260934dcc904d811d
Joshua Williams <jmw0426@hotmail.com>
2020-05-27 19:06:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,6 +46,8 @@ class Song
   end
   
  def self.new_from_filename(filename)
+   @name = []
+   @artist_name = []
    if filename.include?(".mp3")
       array = filename.chomp(".mp3").split(" - ")
       @name << array[0]
